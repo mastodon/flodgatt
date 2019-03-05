7a0b11e2401be50866efc2e38b028bf959e1fab2
Daniel Sockwell <dsockwell@gmail.com>
2019-03-05 08:49:35 -0500

    Add descriptive success messages
    Improves the success messages from "OMG! It worked!" to a descriptive
    string identifying which function was invoked.

--- src/api/http/hashtag.rs
+++ src/api/http/hashtag.rs
@@ -2,9 +2,9 @@ use crate::AppState;
 use actix_web::{HttpRequest, Responder};
 
 pub fn index(_req: HttpRequest<AppState>) -> impl Responder {
-    "OMG! It works!"
+    "placeholder response from hashtag::index"
 }
 
 pub fn local(_req: HttpRequest<AppState>) -> impl Responder {
-    "OMG! It works!"
+    "placeholder response from hashtag::local"
 }
