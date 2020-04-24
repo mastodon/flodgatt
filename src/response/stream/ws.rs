314c8ca7478b36a6d99bf2d1aeb80b40123929c6
Daniel Sockwell <daniel@codesections.com>
2020-04-24 16:21:15 -0400

    Simplify handling of disconnected clients

--- src/response/stream/ws.rs
+++ src/response/stream/ws.rs
@@ -4,11 +4,10 @@ use crate::request::Subscription;
 use futures::future::Future;
 use futures::stream::Stream;
 use std::sync::Arc;
-use tokio::sync::mpsc::{self, Receiver, UnboundedSender};
+use tokio::sync::mpsc::Receiver;
 use warp::ws::{Message, WebSocket};
 
 type EventRx = Receiver<Arc<Event>>;
-type MsgTx = UnboundedSender<Message>;
 
 pub struct Ws(Subscription);
 
@@ -23,45 +22,37 @@ impl Ws {
         event_rx: EventRx,
     ) -> impl Future<Item = (), Error = ()> {
         let (transmit_to_ws, _receive_from_ws) = ws.split();
-        // Create a pipe, send one end of it to a different green thread and tell that end
-        // to forward to the WebSocket client
-        let (mut ws_tx, ws_rx) = mpsc::unbounded_channel();
-        warp::spawn(
-            ws_rx
-                .map_err(|_| -> warp::Error { unreachable!() })
-                .forward(transmit_to_ws)
-                .map(|_r| ())
-                .map_err(|e| {
-                    match e.to_string().as_ref() {
-                        "IO error: Broken pipe (os error 32)" => (), // just closed unix socket
-                        _ => log::warn!("WebSocket send error: {}", e),
+        event_rx
+            .filter_map(move |event| {
+                if matches!(*event, Event::Ping) {
+                    Some(Message::text(&event.to_json_string()))
+                } else {
+                    match (event.update_payload(), event.dyn_update_payload()) {
+                        (Some(update), _) if !self.filtered(update) => {
+                            Some(Message::text(&event.to_json_string()))
+                        }
+                        (None, None) => Some(Message::text(&event.to_json_string())), // send all non-updates
+                        (_, Some(dyn_update)) if !self.filtered(dyn_update) => {
+                            Some(Message::text(&event.to_json_string()))
+                        }
+                        _ => None,
                     }
-                }),
-        );
-
-        event_rx.map_err(|_| ()).for_each(move |event| {
-            if matches!(*event, Event::Ping) {
-                send_msg(&event, &mut ws_tx)?
-            } else {
-                match (event.update_payload(), event.dyn_update_payload()) {
-                    (Some(update), _) => self.send_or_filter(&event, update, &mut ws_tx),
-                    (None, None) => send_msg(&event, &mut ws_tx), // send all non-updates
-                    (_, Some(dyn_update)) => self.send_or_filter(&event, dyn_update, &mut ws_tx),
-                }?
-            }
-            Ok(())
-        })
+                }
+            })
+            .map_err(|_| -> warp::Error { unreachable!() })
+            .forward(transmit_to_ws)
+            .map(|_r| ())
+            .map_err(|e| {
+                match e.to_string().as_ref() {
+                    "IO error: Broken pipe (os error 32)" => log::info!("transmit_to_ws error"), // just closed unix socket
+                    _ => log::warn!("WebSocket send error: {}", e),
+                }
+            })
     }
-
-    fn send_or_filter(
-        &mut self,
-        event: &Event,
-        update: &impl Payload,
-        mut ws_tx: &mut MsgTx,
-    ) -> Result<(), ()> {
+    fn filtered(&mut self, update: &impl Payload) -> bool {
         let (blocks, allowed_langs) = (&self.0.blocks, &self.0.allowed_langs);
 
-        let skip = |reason, tl| Ok(log::info!("{:?} msg skipped - {}", tl, reason));
+        let skip = |reason, tl| Some(log::info!("{:?} msg skipped - {}", tl, reason)).is_some();
 
         match self.0.timeline {
             tl if tl.is_public()
@@ -79,13 +70,7 @@ impl Ws {
             tl if blocks.blocked_domains.contains(update.sent_from()) => {
                 skip("from blocked domain", tl)
             }
-            _ => Ok(send_msg(event, &mut ws_tx)?),
+            _ => false,
         }
     }
-}
-
-fn send_msg(event: &Event, ws_tx: &mut MsgTx) -> Result<(), ()> {
-    ws_tx
-        .try_send(Message::text(&event.to_json_string()))
-        .map_err(|_| log::info!("WebSocket connection closed"))
 }
